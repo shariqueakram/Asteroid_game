4f4ee9ccd3d5318451f568de106f1ccba574b74f
Sharique Akram <shariqueakram@gmail.com>
2025-01-02 12:25:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+	print("Starting asteroids!")
+	print("Screen width:",SCREEN_WIDTH,"\nScreen height:",SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
