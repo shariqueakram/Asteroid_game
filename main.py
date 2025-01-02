af42075a8286e868a44bb2b4918c47be8f3aee83
Sharique Akram <shariqueakram@gmail.com>
2025-01-02 18:51:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
 	pygame.init()    
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))	
 	print("Starting asteroids!")
 	print("Screen width:",SCREEN_WIDTH,"\nScreen height:",SCREEN_HEIGHT)
 
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			
+		screen.fill((0,0,0))
+		pygame.display.flip()
+		
 if __name__ == "__main__":
     main()
