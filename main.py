b6dc8475a48b2c67565e0269fe127e72e9412f9e
Sharique Akram <shariqueakram@gmail.com>
2025-01-02 15:20:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+	pygame.init()    
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print("Screen width:",SCREEN_WIDTH,"\nScreen height:",SCREEN_HEIGHT)
 
