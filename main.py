43304c3ce9838150ad7d17b191aa90f17d6e04a1
Sharique Akram <shariqueakram@gmail.com>
2025-01-06 10:49:50 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))	
 	print("Starting asteroids!")
 	print("Screen width:",SCREEN_WIDTH,"\nScreen height:",SCREEN_HEIGHT)
+	clock = pygame.time.Clock()
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
@@ -17,6 +19,6 @@ def main():
 			
 		screen.fill((0,0,0))
 		pygame.display.flip()
-		
+		dt = clock.tick(60) / 1000	
 if __name__ == "__main__":
     main()
