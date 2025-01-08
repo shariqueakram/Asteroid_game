6e16c7c312ee534c7a335088712338afb71d2e5d
Sharique Akram <shariqueakram@gmail.com>
2025-01-08 10:40:01 -0500

    Asteroids spawning now, no colliders just yet

--- asteroid.py
+++ asteroid.py
@@ -4,9 +4,10 @@ from player import *
 class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
         super().__init__(x,y,radius)
-        self.velocity = pygame.Vector2(0, 0)
+        self.velocity = pygame.Vector2(x, y)
+        self.radius = radius
     def draw(self, surface):
-        pygame.draw.circle(surface,(255,255,255),(self.x,self.y),self.radius,width=2)
+        pygame.draw.circle(surface,(255,255,255),(self.position.x,self.position.y),self.radius,width=2)
     def update(self, dt):
-        self.x += self.velocity.x * dt
-        self.y += self.velocity.y * dt        
+        self.position.x += self.velocity.x * dt
+        self.position.y += self.velocity.y * dt        
